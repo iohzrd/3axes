d7311ad47a8d82d1ec82f71ef3911f010d6ff499
Adam <iohzrd@protonmail.com>
2017-11-04 23:36:58 -0700

    lots of changes

--- ideologies.js
+++ ideologies.js
@@ -1,274 +1,258 @@
-ideologies = [
-    {
-        "name": "Communism",
-        "stats": {
-            "idty": 90,
-            "prop": 90,
-            "cult": 90
-        }
+const ideologies = [
+  {
+    name: 'Communism',
+    stats: {
+      idty: 90,
+      prop: 90,
+      cult: 90,
     },
-    {
-        "name": "National Bolshevism",
-        "stats": {
-            "idty": 15,
-            "prop": 90,
-            "cult": 20
-        }
+  },
+  {
+    name: 'National Bolshevism',
+    stats: {
+      idty: 15,
+      prop: 90,
+      cult: 20,
     },
-    {
-        "name": "Stalinism/Maoism",
-        "stats": {
-            "idty": 30,
-            "prop": 90,
-            "cult": 50
-        }
+  },
+  {
+    name: 'Stalinism/Maoism',
+    stats: {
+      idty: 30,
+      prop: 90,
+      cult: 50,
     },
-    {
-        "name": "Trotskyism",
-        "stats": {
-            "idty": 90,
-            "prop": 90,
-            "cult": 80
-        }
+  },
+  {
+    name: 'Trotskyism',
+    stats: {
+      idty: 90,
+      prop: 90,
+      cult: 80,
     },
-    {
-        "name": "Marxism",
-        "stats": {
-            "idty": 70,
-            "prop": 90,
-            "cult": 80
-        }
+  },
+  {
+    name: 'Marxism',
+    stats: {
+      idty: 70,
+      prop: 90,
+      cult: 80,
     },
-    {
-        "name": "De Leonism",
-        "stats": {
-            "idty": 70,
-            "prop": 90,
-            "cult": 80
-        }
+  },
+  {
+    name: 'De Leonism',
+    stats: {
+      idty: 70,
+      prop: 90,
+      cult: 80,
     },
-    {
-        "name": "Leninism",
-        "stats": {
-            "idty": 60,
-            "prop": 90,
-            "cult": 70
-        }
+  },
+  {
+    name: 'Leninism',
+    stats: {
+      idty: 60,
+      prop: 90,
+      cult: 70,
     },
-    {
-        "name": "State Socialism",
-        "stats": {
-            "idty": 70,
-            "prop": 90,
-            "cult": 70
-        }
+  },
+  {
+    name: 'State Socialism',
+    stats: {
+      idty: 70,
+      prop: 90,
+      cult: 70,
     },
-    {
-        "name": "Democratic Socialism",
-        "stats": {
-            "idty": 50,
-            "prop": 90,
-            "cult": 80
-        }
+  },
+  {
+    name: 'Democratic Socialism',
+    stats: {
+      idty: 50,
+      prop: 90,
+      cult: 80,
     },
-    {
-        "name": "Revolutionary Socialism",
-        "stats": {
-            "idty": 80,
-            "prop": 90,
-            "cult": 90
-        }
+  },
+  {
+    name: 'Revolutionary Socialism',
+    stats: {
+      idty: 80,
+      prop: 90,
+      cult: 90,
     },
-    {
-        "name": "Libertarian Socialism",
-        "stats": {
-            "idty": 20,
-            "prop": 80,
-            "cult": 20
-        }
+  },
+  {
+    name: 'Anarcho-Syndicalism',
+    stats: {
+      idty: 50,
+      prop: 75,
+      cult: 20,
     },
-    {
-        "name": "Anarcho-Syndicalism",
-        "stats": {
-            "idty": 50,
-            "prop": 75,
-            "cult": 20
-        }
+  },
+  {
+    name: 'Left-Wing Populism',
+    stats: {
+      idty: 50,
+      prop: 75,
+      cult: 65,
     },
-    {
-        "name": "Left-Wing Populism",
-        "stats": {
-            "idty": 50,
-            "prop": 75,
-            "cult": 65
-        }
+  },
+  {
+    name: 'Distributism',
+    stats: {
+      idty: 50,
+      prop: 75,
+      cult: 25,
     },
-    {
-        "name": "Right-Wing Populism",
-        "stats": {
-            "idty": 50,
-            "prop": 25,
-            "cult": 35
-        }
+  },
+  {
+    name: 'Progressivism',
+    stats: {
+      idty: 80,
+      prop: 65,
+      cult: 80,
     },
-    {
-        "name": "Distributism",
-        "stats": {
-            "idty": 50,
-            "prop": 75,
-            "cult": 80
-        }
+  },
+  {
+    name: 'Mutualism',
+    stats: {
+      idty: 50,
+      prop: 60,
+      cult: 50,
     },
-    {
-        "name": "Progressivism",
-        "stats": {
-            "idty": 80,
-            "prop": 65,
-            "cult": 80
-        }
+  },
+  {
+    name: 'Neo-Liberalism',
+    stats: {
+      idty: 70,
+      prop: 60,
+      cult: 60,
     },
-    {
-        "name": "Anarcho-Mutualism",
-        "stats": {
-            "idty": 50,
-            "prop": 60,
-            "cult": 30
-        }
+  },
+  {
+    name: 'Technocracy',
+    stats: {
+      idty: 90,
+      prop: 55,
+      cult: 90,
     },
-    {
-        "name": "Technocracy",
-        "stats": {
-            "idty": 90,
-            "prop": 55,
-            "cult": 90
-        }
+  },
+  {
+    name: 'Radical Centrist',
+    stats: {
+      idty: 50,
+      prop: 50,
+      cult: 50,
     },
-    {
-        "name": "Corporatism",
-        "stats": {
-            "idty": 80,
-            "prop": 25,
-            "cult": 50
-        }
+  },
+  {
+    name: 'Liberalism',
+    stats: {
+      idty: 60,
+      prop: 50,
+      cult: 60,
     },
-    {
-        "name": "Radical Centrist",
-        "stats": {
-            "idty": 50,
-            "prop": 50,
-            "cult": 50
-        }
+  },
+  {
+    name: 'National Socialism',
+    stats: {
+      idty: 25,
+      prop: 50,
+      cult: 25,
     },
-    {
-        "name": "Liberalism",
-        "stats": {
-            "idty": 60,
-            "prop": 50,
-            "cult": 60
-        }
+  },
+  {
+    name: 'Exclusive Democracy',
+    stats: {
+      idty: 25,
+      prop: 35,
+      cult: 45,
     },
-    {
-        "name": "Moderate Conservatism",
-        "stats": {
-            "idty": 50,
-            "prop": 45,
-            "cult": 40
-        }
+  },
+  {
+    name: 'Neo-Conservatism',
+    stats: {
+      idty: 55,
+      prop: 50,
+      cult: 55,
     },
-    {
-        "name": "Social Libertarianism",
-        "stats": {
-            "idty": 70,
-            "prop": 35,
-            "cult": 70
-        }
+  },
+  {
+    name: 'Moderate Conservatism',
+    stats: {
+      idty: 50,
+      prop: 45,
+      cult: 40,
     },
-    {
-        "name": "Libertarianism",
-        "stats": {
-            "idty": 50,
-            "prop": 25,
-            "cult": 50
-        }
+  },
+  {
+    name: 'Conservatism',
+    stats: {
+      idty: 50,
+      prop: 40,
+      cult: 50,
     },
-    {
-        "name": "Anarcho-Egoism",
-        "stats": {
-            "idty": 90,
-            "prop": 0,
-            "cult": 90
-        }
+  },
+  {
+    name: 'Capitalist Fascism',
+    stats: {
+      idty: 25,
+      prop: 25,
+      cult: 15,
     },
-    {
-        "name": "National Socialism",
-        "stats": {
-            "idty": 25,
-            "prop": 50,
-            "cult": 25
-        }
+  },
+  {
+    name: 'Right-Wing Populism',
+    stats: {
+      idty: 50,
+      prop: 25,
+      cult: 35,
     },
-    {
-        "name": "Capitalist Fascism",
-        "stats": {
-            "idty": 25,
-            "prop": 25,
-            "cult": 15
-        }
+  },
+  {
+    name: 'Corporatocracy',
+    stats: {
+      idty: 80,
+      prop: 25,
+      cult: 50,
     },
-    {
-        "name": "Conservatism",
-        "stats": {
-            "idty": 50,
-            "prop": 40,
-            "cult": 50
-        }
+  },
+  {
+    name: 'Libertarianism',
+    stats: {
+      idty: 50,
+      prop: 25,
+      cult: 50,
     },
-    {
-        "name": "Neo-Liberalism",
-        "stats": {
-            "idty": 70,
-            "prop": 60,
-            "cult": 60
-        }
+  },
+  {
+    name: 'National Capitalism',
+    stats: {
+      idty: 30,
+      prop: 20,
+      cult: 30,
     },
-    {
-        "name": "Classical Liberalism",
-        "stats": {
-            "idty": 30,
-            "prop": 15,
-            "cult": 25
-        }
+  },
+  {
+    name: 'Classical Liberalism',
+    stats: {
+      idty: 30,
+      prop: 15,
+      cult: 25,
     },
-    {
-        "name": "Neo-Conservatism",
-        "stats": {
-            "idty": 55,
-            "prop": 50,
-            "cult": 55
-        }
+  },
+  {
+    name: 'Anarcho-Egoism',
+    stats: {
+      idty: 90,
+      prop: 0,
+      cult: 90,
     },
-    {
-        "name": "National Capitalism",
-        "stats": {
-            "idty": 30,
-            "prop": 20,
-            "cult": 30
-        }
+  },
+  {
+    name: 'Anarcho-Capitalism',
+    stats: {
+      idty: 15,
+      prop: 0,
+      cult: 15,
     },
-    {
-        "name": "Libertarian Capitalism",
-        "stats": {
-            "idty": 40,
-            "prop": 15,
-            "cult": 20
-        }
-    },
-    {
-        "name": "Anarcho-Capitalism",
-        "stats": {
-            "idty": 15,
-            "prop": 0,
-            "cult": 15
-        }
-    }
+  },
 ];
---
+++ quiz.js
@@ -0,0 +1,66 @@
+const doc = window.document;
+
+let maxIdentity = 0;
+let maxProperty = 0;
+let maxCulture = 0;
+let idty = 0;
+let prop = 0;
+let cult = 0;
+let qn = 0;
+let previousAnswer = null;
+
+for (let i = 0; i < questions.length; i++) {
+  maxIdentity += Math.abs(questions[i].effect.idty);
+  maxProperty += Math.abs(questions[i].effect.prop);
+  maxCulture += Math.abs(questions[i].effect.cult);
+}
+
+function calcScore(score, max) {
+  return ((100 * (max + score)) / (2 * max)).toFixed(1);
+}
+
+function results() {
+  location.href = 'results.html'
+    + `?i=${calcScore(idty, maxIdentity)}`
+    + `&p=${calcScore(prop, maxProperty)}`
+    + `&c=${calcScore(cult, maxCulture)}`;
+}
+
+function initQuestion() {
+  doc.getElementById('question-text').innerHTML = questions[qn].question;
+  doc.getElementById('question-number').innerHTML = `Question ${qn + 1} of ${questions.length}`;
+  if (previousAnswer == null) {
+    doc.getElementById('back_button').style.display = 'none';
+    doc.getElementById('back_button_off').style.display = 'block';
+  } else {
+    doc.getElementById('back_button').style.display = 'block';
+    doc.getElementById('back_button_off').style.display = 'none';
+  }
+}
+
+function next_question(mult) {
+  idty += mult * questions[qn].effect.idty;
+  prop += mult * questions[qn].effect.prop;
+  cult += mult * questions[qn].effect.cult;
+  qn += 1;
+  previousAnswer = mult;
+  if (qn < questions.length) {
+    initQuestion();
+  } else {
+    results();
+  }
+}
+
+function prev_question() {
+  if (previousAnswer == null) {
+    return;
+  }
+  qn -= 1;
+  idty -= previousAnswer * questions[qn].effect.idty;
+  prop -= previousAnswer * questions[qn].effect.prop;
+  cult -= previousAnswer * questions[qn].effect.cult;
+  previousAnswer = null;
+  initQuestion();
+}
+
+initQuestion();
